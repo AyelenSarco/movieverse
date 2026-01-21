156e9f75e93875859b89d3113ee2cc9d0a79e078
Ayelen Marina Sarco <ayelensarco94@gmail.com>
2026-01-21 17:31:04 -0300

    Initial project setup

---
+++ src/main/java/com/movieverse/movieverse_api/MovieverseApiApplication.java
@@ -0,0 +1,13 @@
+package com.movieverse.movieverse_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieverseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieverseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movieverse/movieverse_api/MovieverseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieverse.movieverse_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieverseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
